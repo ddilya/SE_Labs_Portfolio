4491843491a3b99a9cf162b8cf21ee4d4506db45
sunie582 <elbasy_s@mail.ru>
2026-04-21 02:30:28 +0500

    RED: Add test for all ones

--- test_bow.py
+++ test_bow.py
@@ -10,9 +10,12 @@ class TestBowlingGame(unittest.TestCase):
             self.g.roll(pins)
 
     def test_gutter_game(self):
-        """Тест: все броски в ноль (Gutter Game)"""
         self.roll_many(20, 0)
         self.assertEqual(self.g.score(), 0)
+
+    def test_all_ones(self):
+        self.roll_many(20, 1)
+        self.assertEqual(self.g.score(), 20)
 
 if __name__ == '__main__':
     unittest.main()
