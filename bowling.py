cae5b7b99d674a6f91c873c258fcc9641381df81
sunie582 <elbasy_s@mail.ru>
2026-04-21 02:33:05 +0500

    GREEN: Implement spare logic

--- bowling.py
+++ bowling.py
@@ -6,4 +6,16 @@ class Game:
         self._rolls.append(pins)
 
     def score(self):
-        return sum(self._rolls)
+        total_score = 0
+        roll_index = 0
+        for frame in range(10):
+            if self._is_spare(roll_index):
+                total_score += 10 + self._rolls[roll_index + 2]
+                roll_index += 2
+            else:
+                total_score += self._rolls[roll_index] + self._rolls[roll_index + 1]
+                roll_index += 2
+        return total_score
+
+    def _is_spare(self, roll_index):
+        return self._rolls[roll_index] + self._rolls[roll_index + 1] == 10
